5c8083a46aa057ed50a9036bf807d2f2d2ee666b
Juan Narvaez <juand.ns07@gmail.com>
2023-07-17 11:39:55 -0300

    config PORT

--- src/index.js
+++ src/index.js
@@ -4,8 +4,10 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+const PORT = process.env.PORT || 3000;
+
 connectDB();
 
-app.listen(3000, () => {
-	console.log("Server is running on port", 3000);
+app.listen(PORT, () => {
+	console.log("Server is running on port", PORT);
 });
